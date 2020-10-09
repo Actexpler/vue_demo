d1350feac3854b4c5fbc3d98d74ab1699d510ef4
wangzongcheng <18713589711@163.com>
2020-10-09 22:24:30 +0800

    change

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,15 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: wangzongcheng
+ * @Date: 2019-10-16 16:14:35
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-10-09 22:24:11
+ */
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Index from '../components/pages/Index'
+import Index from '../components/pages/index'
 import userCenter from '../components/pages/userCenter'
 import userInfo from '../components/pages/userInfo'
 import Login from '../components/login'
